7f5fc8bdeec7a95db8dde5820cddb2ace847f4e2
Vasily Shamporov <vasily.shamporov@intel.com>
2018-09-14 19:03:25 +0300

    [ICL][VP9d] Add the Y410 decoding option for VP9 on ICL
    This change should be a starting point for enabling VP9 Y410
    decoding on ICL

--- _studio/mfx_lib/shared/src/mfx_vpx_dec_common.cpp
+++ _studio/mfx_lib/shared/src/mfx_vpx_dec_common.cpp
@@ -319,7 +319,7 @@ namespace MFX_VPX_Utility
         return sts;
     }
 
-    bool CheckVideoParam(mfxVideoParam const*p_in, mfxU32 codecId, eMFXPlatform platform)
+    bool CheckVideoParam(mfxVideoParam const*p_in, mfxU32 codecId, eMFXPlatform platform, eMFXHWType type)
     {
         if (!p_in)
             return false;
@@ -380,7 +380,7 @@ namespace MFX_VPX_Utility
             if (MFX_FOURCC_NV12 != p_in->mfx.FrameInfo.FourCC &&
                 MFX_FOURCC_AYUV != p_in->mfx.FrameInfo.FourCC &&
                 MFX_FOURCC_P010 != p_in->mfx.FrameInfo.FourCC &&
-                MFX_FOURCC_Y410 != p_in->mfx.FrameInfo.FourCC)
+                !(MFX_FOURCC_Y410 == p_in->mfx.FrameInfo.FourCC && (type == MFX_HW_ICL || type == MFX_HW_ICL_LP)))
                 return false;
             if (MFX_CHROMAFORMAT_YUV420 != p_in->mfx.FrameInfo.ChromaFormat &&
                 MFX_CHROMAFORMAT_YUV444 != p_in->mfx.FrameInfo.ChromaFormat)
